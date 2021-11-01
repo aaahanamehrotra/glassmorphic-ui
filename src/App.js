c063df60aac477bf475fd11999e5eec5f4de573c
aaahana <e10991aaahana@dpsrkp.net>
2021-11-01 18:44:31 +0530

    add app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
-
-
+// import Hero from "./components/Hero/Hero";
+import './app.scss'
+// import Navbar from "./components/Navbar/Navbar";
+// import { BrowserRouter, Route, Link } from "react-router-dom";
+// import Home from "./pages/Home";
 function App() {
   return (
-<h1>hello</h1>
+    <div className="app">
+    {/* <BrowserRouter>
+      <Route exact path='/' component={Home}></Route>
+    </BrowserRouter> */}
+  {/* <Navbar /> */}
+  app
+    </div>
   );
 }
 
